89783d8261f345c6207cdd06937ed17244081741
Aaron Kearly <kearlyaa@gmail.com>
2016-05-21 14:12:18 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kittycat::Application.config.secret_key_base = 'dfcfe8776f8885d0b4e0c9c011c13e19b1e3d998f1f7e83d54c6697e960bd8bf5bf9838f4aa4bf556ef61f204a592673c35d0c492a56fffe17ac64f5ce2c31f1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kittycat::Application.config.session_store :cookie_store, key: '_kittycat_session'
